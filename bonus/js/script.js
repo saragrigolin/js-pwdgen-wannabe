81b089ef163f688369d6b29a4e6a2201bab0fafa
saragrigolin <56165909+saragrigolin@users.noreply.github.com>
2021-11-19 13:32:49 +0100

    edit let in const

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -11,19 +11,19 @@
 
 
 // Chiedo all'utente il suo nome
-let nomeUtente = prompt('Inserisci il tuo nome');
+const nomeUtente = prompt('Inserisci il tuo nome');
 console.log('Nome utente ' + nomeUtente);
 
 // Chiedo all'utente il suo cognome
-let cognomeUtente = prompt('Inserisci il tuo cognome');
+const cognomeUtente = prompt('Inserisci il tuo cognome');
 console.log('Cognome utente', cognomeUtente);
 
 // Chiedo all'utente il suo colore preferito
-let colorePreferitoUtente = prompt('Inserisci il tuo colore preferito');
+const colorePreferitoUtente = prompt('Inserisci il tuo colore preferito');
 console.log('Colore preferito utente ' + colorePreferitoUtente);
 
 // Chiedo all'utente il suo numero preferito
-let numeroPreferitoUtente = prompt('Inserisci il tuo numero preferito');
+const numeroPreferitoUtente = prompt('Inserisci il tuo numero preferito');
 console.log ('Numero preferito utente', numeroPreferitoUtente);
 
 // Sommo 5 al numero preferito
--- js/script.js
+++ js/script.js
@@ -5,19 +5,19 @@
 // Infine scrivi sulla pagina nomecognomecolorepreferito21
 
 // Chiedo all'utente il suo nome
-let nomeUtente = prompt('Inserisci il tuo nome');
+const nomeUtente = prompt('Inserisci il tuo nome');
 console.log('Nome utente ' + nomeUtente);
 
 // Chiedo all'utente il suo cognome
-let cognomeUtente = prompt('Inserisci il tuo cognome');
+const cognomeUtente = prompt('Inserisci il tuo cognome');
 console.log('Cognome utente', cognomeUtente);
 
 // Chiedo all'utente il suo colore preferito
-let colorePreferitoUtente = prompt('Inserisci il tuo colore preferito');
+const colorePreferitoUtente = prompt('Inserisci il tuo colore preferito');
 console.log('Colore preferito utente ' + colorePreferitoUtente);
 
 // Variabile per numero 21
-let number = 21;
+const number = 21;
 
 // Scrivo sulla pagina nomecognomecolorepreferito21
 let password = nomeUtente + cognomeUtente + colorePreferitoUtente + number;
